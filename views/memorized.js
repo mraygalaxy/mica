e2b75cd1d45e91ada2b719f902f38b7b6654b28e
Michael R. Galaxy <michael@flatgalaxy.com>
2014-06-03 08:56:28 +0800

    main page loading is much faster now

--- views/memorized.js
+++ views/memorized.js
@@ -5,6 +5,9 @@
        "allcount": {
            "map": "function(doc) { if (doc._id.match(/MICA:[^:]+:memorized:[^:]+$/g).length == 1) { emit([doc._id.replace(/(MICA:|:memorized:.*)/g, ''), doc._id.replace(/(MICA:[^:]+:memorized:)/g, '')], doc); } }",
            "reduce": "_count"
+       },
+       "all": {
+           "map": "function(doc) { if (doc._id.match(/MICA:[^:]+:memorized:[^:]+$/g).length == 1) { emit([doc._id.replace(/(MICA:|:memorized:.*)/g, ''), doc._id.replace(/(MICA:[^:]+:memorized:)/g, '')], doc); } }"
        }
    }
 }
