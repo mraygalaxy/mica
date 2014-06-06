4ac66e78c76b40f2af77ae1823176b95b6d667fb
Michael R. Hines <mrhines@us.ibm.com>
2014-06-06 12:54:57 +0800

    adapter to switch between couch and mobile version in python

--- views/tonechanges.js
+++ views/tonechanges.js
@@ -3,7 +3,7 @@
    "language": "javascript",
    "views": {
        "all": {
-           "map": "function(doc) { if (doc._id.match(/MICA:[^:]+:tonechanges:[^:]+$/g).length == 1) { emit([doc._id.replace(/(MICA:|:tonechanges:.*)/g, ''), doc._id.replace(/(MICA:[^:]+:tonechanges:)/g, '')], doc); } }"
+           "map": "function(doc) { if (doc._id.match(/MICA:[^:]+:tonechanges:[^:]+$/g).length == 1) { emit([doc._id.replace(/(MICA:|:tonechanges:.*)/g, ''), doc._id.replace(/MICA:[^:]+:tonechanges:/g, '')], doc); } }"
        }
    }
 }
