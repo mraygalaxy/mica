5ad21f2c71ef9099560f1da801cf37c77d482750
Michael R. Galaxy <michael@flatgalaxy.com>
2014-06-09 02:23:08 +0800

    errors in views

--- views/mergegroups.js
+++ views/mergegroups.js
@@ -3,7 +3,7 @@
    "language": "javascript",
    "views": {
        "all": {
-           "map": "function(doc) { if (doc._id.match(/MICA:[^:]+:mergegroups:[^:]+$/g).length == 1) { emit([doc._id.replace(/(MICA:|:mergegroups:.*)/g, ''), doc._id.replace(/(MICA:[^:]+:mergegroups:)/g, '')], doc); } }"
+           "map": "function(doc) { if (/MICA:[^:]+:mergegroups:[^:]+$/.test(doc._id)) { emit([doc._id.replace(/(MICA:|:mergegroups:.*)/g, ''), doc._id.replace(/(MICA:[^:]+:mergegroups:)/g, '')], doc); } }"
        }
    }
 }
