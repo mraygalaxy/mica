0c6851507377ff6c47161fb0c633a0f60809ad8b
Michael R. Hines <michael@hinespot.com>
2014-06-11 17:29:14 +0800

    views are much simpler now. Need to be tested, but much simpler

--- views/splits.js
+++
@@ -1,9 +0,0 @@
-{
-   "_id": "_design/splits",
-   "language": "javascript",
-   "views": {
-       "all": {
-           "map": "function(doc) { if (/MICA:[^:]+:splits:[^:]+$/.test(doc._id)) { emit([doc._id.replace(/(MICA:|:splits:.*)/g, ''), doc._id.replace(/(MICA:[^:]+:splits:)/g, '')], doc); } }"
-       }
-   }
-}
