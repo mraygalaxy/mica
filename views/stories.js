03b685d0de4cc834c4073ad588d6ae306c4a58c4
Michael R. Galaxy <michael@flatgalaxy.com>
2014-06-21 11:27:18 +0800

    wow, so many bugs

--- views/stories.js
+++ views/stories.js
@@ -1,25 +1,25 @@
 {
    "language": "javascript",
    "views": {
+       "all": {
+           "map": "function(doc) { if (/MICA:[^:]+:stories:[^:]+$/.test(doc._id)) { emit([doc._id.replace(/(MICA:|:stories:.*)/g, ''), doc.name], doc); } }"
+       },
+       "allpages": {
+           "map": "function(doc) { if (/MICA:[^:]+:stories:[^:]+:pages:[^:]+$/.test(doc._id)) { emit([doc._id.replace(/(MICA:|:stories:.*)/g, ''), doc._id.replace(/(MICA:[^:]+:stories:|:pages:.*)/g, ''), doc._id.replace(/MICA:[^:]+:stories:[^:]+:pages:/g, '')], doc); } }"
+       },
+       "original": {
+           "map": "function(doc) { if (/MICA:[^:]+:stories:[^:]+:original:[^:]+$/.test(doc._id)) { emit([doc._id.replace(/(MICA:|:stories:.*)/g, ''), doc._id.replace(/(MICA:[^:]+:stories:|:original:.*)/g, ''), doc._id.replace(/MICA:[^:]+:stories:[^:]+:original:/g, '')], doc); } }",
+           "reduce": "function(keys, values, rereduce) { if (rereduce) { return sum(values); } else { return values.length; } }"
+       },
        "translating": {
            "map": "function(doc) { if (/MICA:[^:]+:stories:[^:]+$/.test(doc._id) && (doc.translating != undefined) && (doc.translating == true)) { emit([doc._id.replace(/(MICA:|:stories:.*)/g, ''), doc.name], doc); } }"
        },
-         "all": {
-           "map": "function(doc) { if (/MICA:[^:]+:stories:[^:]+$/.test(doc._id)) { emit([doc._id.replace(/(MICA:|:stories:.*)/g, ''), doc.name], doc); } }"
+       "alloriginal": {
+           "map": "function(doc) { if (/MICA:[^:]+:stories:[^:]+:original:[^:]+$/.test(doc._id)) { emit([doc._id.replace(/(MICA:|:stories:.*)/g, ''), doc._id.replace(/(MICA:[^:]+:stories:|:original:.*)/g, ''), doc._id.replace(/MICA:[^:]+:stories:[^:]+:original:/g, '')], doc); } }"
        },
        "pages": {
-           "map": "function(doc) { if (/pages:[^:]+$/.test(doc._id)) { emit(doc._id.replace(/pages:/g, ''), doc); } }",
+           "map": "function(doc) { if (/MICA:[^:]+:stories:[^:]+:pages:[^:]+$/.test(doc._id)) { emit([doc._id.replace(/(MICA:|:stories:.*)/g, ''), doc._id.replace(/(MICA:[^:]+:stories:|:pages:.*)/g, ''), doc._id.replace(/MICA:[^:]+:stories:[^:]+:pages:/g, '')], doc); } }",
            "reduce": "function(keys, values, rereduce) { if (rereduce) { return sum(values); } else { return values.length; } }"
-       },
-       "allpages": {
-           "map": "function(doc) { if (/pages:[^:]+$/.test(doc._id)) { emit(doc._id.replace(/pages:/g, ''), doc); } }"
-       },
-       "original": {
-           "map": "function(doc) { if (/original:[^:]+$/.test(doc._id)) { emit(doc._id.replace(/original:/g, ''), doc); } }",
-           "reduce": "function(keys, values, rereduce) { if (rereduce) { return sum(values); } else { return values.length; } }"
-       },
-       "alloriginal": {
-           "map": "function(doc) { if (/original:[^:]+$/.test(doc._id)) { emit(doc._id.replace(/original:/g, ''), doc); } }"
        }
-   }
+   }   
 }
