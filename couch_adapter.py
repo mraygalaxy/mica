c6c0771349254291340dfa07b462ebb4a49da4b3
Michael R. Hines <hinesmr@cn.ibm.com>
2014-08-21 00:20:49 +0800

    successfully deployed on iOS

--- couch_adapter.py
+++ couch_adapter.py
@@ -13,7 +13,10 @@ try :
     from jnius import autoclass
     String = autoclass('java.lang.String')
 except ImportError, e :
-    mdebug("pyjnius not available. Probably on a server.")
+    try :
+        from pyobjus import autoclass, objc_f, objc_str as String, objc_l as Long, objc_i as Integer
+    except ImportError, e :
+        mdebug("pyjnius and pyobjus not available. Probably on a server.")
 
 def couchdb_pager(db, view_name='_all_docs',
                   startkey=None, startkey_docid=None,
@@ -206,11 +209,11 @@ class MicaServerCouchDB(object) :
     def __contains__(self, dbname) :
         return True if dbname in self.server else False
 
-class MicaDatabaseCouchbaseMobile(object) :
+class AndroidMicaDatabaseCouchbaseMobile(object) :
     def __init__(self, db) :
         self.db = db
         self.dbname = 'mica'
-        mdebug("CouchBase Mobile python adapter initialized")
+        mdebug("Android CouchBase Mobile python adapter initialized")
 
     def __setitem__(self, name, doc) :
         try :
@@ -341,13 +344,173 @@ class MicaDatabaseCouchbaseMobile(object) :
         except Exception, e :
             raise CommunicationError("Database close failed for: " + name)
 
-class MicaServerCouchbaseMobile(object) :
+class AndroidMicaServerCouchbaseMobile(object) :
     def __init__(self, db_already_local) :
         self.db = db_already_local
 
     def __getitem__(self, dbname) :
         self.dbname = dbname
         return MicaDatabaseCouchbaseMobile(self.db)
+
+    def __delitem__(self, name) :
+        try :
+            self.db.drop(name)
+        except Exception, e :
+            raise CommunicationError("Database deletion failed for: " + name)
+
+    def __contains__(self, dbname) :
+        return True if self.db.exists(dbname) else False
+
+class iosMicaDatabaseCouchbaseMobile(object) :
+    def __init__(self, db) :
+        self.db = db
+        self.dbname = 'mica'
+        mdebug("ios CouchBase Mobile python adapter initialized")
+
+    def __setitem__(self, name, doc) :
+        try :
+            err = self.db.put___(self.dbname, name, String(json.dumps(doc))).UTF8String()
+            if err != "" :
+                raise CommunicationError("Error occured putting document: " + name + " " + err)
+        except Exception, e :
+            raise CommunicationError("Error occured putting document: " + str(e), e)
+
+    def __getitem__(self, name) :
+        try :
+            doc = self.db.get__(String(self.dbname), String(name)).UTF8String()
+            mdebug("Result of get is: " + str(doc) + " " + str(type(doc)))
+            if doc == "" :
+                return False
+            if doc is not None :
+                return json.loads(doc)
+        except Exception, e :
+            raise CommunicationError("Error occured getting document: " + name + " " + str(e), e)
+
+        # return was None (null)
+        raise CommunicationError("Bad exception occured getting document: " + name)
+
+    def __delitem__(self, name) :
+        try :
+            err = self.db.delete__(String(self.dbname), String(name)).UTF8String()
+        except Exception, e :
+            raise CommunicationError("Error occured deleting document: " + name + " " + str(e), e)
+        if err != "" :
+            raise ResourceNotFound("Could not delete document: " + name + " " + err)
+
+    def put_attachment(self, name, filename, contents, doc = False) :
+        raise NotImplementedError("Sorry, the mobile version does not allow importing new stories, so creating new attachments is not required today.")
+
+    def get_attachment(self, name, filename) :
+        try :
+            attach = self.db.get_attachment___(String(self.dbname), String(name), String(filename)).UTF8String()
+        except Exception, e :
+            raise CommunicationError("Error getting attachment: " + name + " " + str(e), e)
+        if attach is None :
+            raise ResourceNotFound("Could not find attachment for document: " + name)
+        print "Pyobjus returned attachment of type: " + str(type(attach))
+        return attach 
+
+    def doc_exist(self, name) :
+        try :
+            result = self.db.doc_exist__(String(self.dbname), String(name)).UTF8String()
+            if result == "error" :
+                raise CommunicationError("Error occured checking document existence: " + name)
+            return True if result == "true" else False
+        except Exception, e :
+            raise CommunicationError("Could test document existence: " + name + " " + str(e), e)
+
+    def view(self, name, startkey = False, endkey = False, keys = False, stale = False, username = False) :
+        seed = False
+        uuid = False
+        err_msg = False
+        e = False
+
+        try :
+            parts = name.split("/")
+            assert(len(parts) == 2)
+            design = parts[0]
+            vname = parts[1]
+            params = {}
+            if startkey :
+                params["startkey"] = startkey
+            if endkey :
+                params["endkey"] = endkey
+            if keys :
+                uuid = str(uuid4.uuid4())
+                for key in keys :
+                    assert(isinstance(key, str) or isinstance(key, unicode))
+                    try :
+                        self.db.view_seed___(String(uuid), String(username), String(key))
+                    except Exception, e :
+                        raise CommunicationError("Failed to seed keys for view: " + str(e))
+
+                    seed = True
+
+                params["keys"] = uuid 
+            if stale :
+                params["stale"] = stale
+
+            if len(params) == 0 :
+                params = ""
+            else :
+                params = json.dumps(params)
+
+            try :
+                it = self.db.view____(String(self.dbname), String(design), String(vname), String(params), String(str(username)))
+                total = it.count
+            except Exception, e :
+                raise CommunicationError("Could not get queryrow count: " + str(e))
+
+            if total != 0 :
+                mdebug("There are " + str(total) + " rows in the view results.")
+
+                for vx in range(0, total) :
+                    row = it.rowAtIndex_(Integer(vx).intValue())
+                    result_obj = self.db.view_next_(row)
+
+                    try :
+                        result = result_obj.UTF8String() 
+                    except Exception, e :
+                        raise CommunicationError("Could string from result: " + str(e))
+
+                    j = json.loads(result)
+                    yield j["result"]
+        except Exception, err :
+            err_msg = "Error getting view: " + name + " " + str(err)
+        except CommunicationError, e :
+            err_msg = str(err) 
+        finally :
+            if seed and uuid:
+                self.db.view_seed_cleanup_(String(uuid))
+            if err_msg :
+                raise CommunicationError(err_msg)
+                
+    def compact(self, *args, **kwargs) :
+        if len(args) > 0 :
+            mwarn("Compacting a CBL view doesn't exist. Just pass.")
+            return
+        result = self.db.compact_(self.dbname).UTF8String()
+        if result != "" :
+            raise CommunicationError("Compaction failed: " + result)
+
+    def cleanup(self, *args, **kwargs) :
+        # Does couchbase mobile have this?
+        #self.db.cleanup(*args, **kwargs)
+        pass
+
+    def close(self) :
+        try :
+            self.db.close(self.dbname)
+        except Exception, e :
+            raise CommunicationError("Database close failed for: " + name)
+
+class iosMicaServerCouchbaseMobile(object) :
+    def __init__(self, db_already_local) :
+        self.db = db_already_local
+
+    def __getitem__(self, dbname) :
+        self.dbname = dbname
+        return iosMicaDatabaseCouchbaseMobile(self.db)
 
     def __delitem__(self, name) :
         try :
