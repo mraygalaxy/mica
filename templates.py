2c15f7d52eb23c6fa28af6c17c8767ee897f310e
Michael R. Hines <michael@hinespot.com>
2014-10-23 21:57:00 +0800

    chinese services are next

--- templates.py
+++ templates.py
@@ -8,8 +8,10 @@ from cStringIO import StringIO
 from common import *
 from os import path as os_path
 from re import compile as re_compile
-from requests_oauthlib import OAuth2Session
-from requests_oauthlib.compliance_fixes import facebook_compliance_fix
+
+if not mobile :
+    from requests_oauthlib import OAuth2Session
+    from requests_oauthlib.compliance_fixes import facebook_compliance_fix
 
 cwd = re_compile(".*\/").search(os_path.realpath(__file__)).group(0)
 
