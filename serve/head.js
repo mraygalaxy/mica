8c8f365a3072812c30e430f2e2c3c6b7003dd0d4
Michael R. Galaxy <michael@flatgalaxy.com>
2015-07-28 00:25:34 -0500

    lot of clean-slate progress

--- serve/head.js
+++ serve/head.js
@@ -37,70 +37,21 @@ function switchlist() {
    listreload(current_mode, current_uuid, current_page);
 }
 
-var ConnectisVisible = false;
-var ConnectclickedAway = false;
-function getTemplate() {
-        if ($.browser.device == true)
-                return "<div style='margin-right: 150px' id='connectpopover' class='popover'><div class='arrow'></div><div class='popover-inner'><h3 class='popover-title'></h3><div class='popover-content'><p></p></div></div></div>";
-        else
-                return "<div id='connectpopover' class='popover'><div class='arrow'></div><div class='popover-inner'><h3 class='popover-title'></h3><div class='popover-content'><p></p></div></div></div>";
-}
-var connectpop = $('#connectpop').popover({ 
-    html : true,
-    title: "Connect:",
-    placement : 'bottom',
-    trigger: 'manual',
-    template: getTemplate(),
-    content: function() {
-      return $('#ConnectContents').html();
-    }
-  });
+$("[data-role='header'],[data-role='footer']").toolbar();
+$("[data-role=panel]").panel().enhanceWithin();
 
-   ConnectclickedAway = true;
-   ConnectisVisible = true;
-
-  connectpop.click(function(e) {
-        $(this).popover('toggle');
-        ConnectclickedAway = false;
-        ConnectisVisible = true;
-        e.preventDefault();
-    });
-
-$(document).click(function(e) {
-     if (typeof WL == 'undefined') {
-          var src = e.target;
-     } else {
-          var src = e.srcElement;
-     }
-     if(findParentNode('ConnectContents', src, 'cbody') == true) {
-         return;
-     }
-     if(findParentNode('connectpopover', src, 'cbody') == true) {
-         return;
-     }
-
-     if(ConnectisVisible & ConnectclickedAway)
-     {
-        $('#connectpop').popover('hide');
-        ConnectisVisible = ConnectclickedAway = false;
-     } else {
-        ConnectclickedAway = true;
-     }
-});
-
-$(document).ready(function(){
-    $("div.view1").hide();
-    
-    $("div.slide1").click(function(){
-        $("div.view1").slideToggle(400);
-        $("div.tri1").toggleClass("toggle1");
-    });
-    
-});
+//$(document).bind("mobileinit", function(){ $.extend(  $.mobile , { ajaxEnabled: false });});
 
 /*
 $.mobile.loading("hide");
 $(".ui-loader").hide();
 */
 
-$("#sidebarcontents").panel({beforeopen: function(event, ui) {loadstories(false);}});
+//$("#sidebarcontents").panel({beforeopen: function(event, ui) {loadstories(false);}});
+
+$(document).on("pagecreate", function () {
+    $("[data-role=panel]").one("panelbeforeopen", function () {
+        var height = $.mobile.pageContainer.pagecontainer("getActivePage").outerHeight();
+        $(".ui-panel-wrapper").css("height", height + 1);
+    });
+});
