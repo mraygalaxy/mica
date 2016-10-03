73cc448d172441a277de4568c721428a4c66f27c
Michael R. Galaxy <michael@flatgalaxy.com>
2016-10-03 17:40:06 -0500

    trying to pull out pings

--- serve/mica.js
+++ serve/mica.js
@@ -1659,7 +1659,7 @@ function doLogin(oForm) {
 
     var server = oForm.server.value, oArgs = new Object();
 
-    oDbg = new JSJaCConsoleLogger(2);
+    oDbg = new JSJaCConsoleLogger(1);
     document.getElementById('err').innerHTML = '';
     // reset
 
