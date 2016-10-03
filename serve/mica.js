88ad4717cd41ad60d6494db890ce0fc18a75e0c9
Michael R. Galaxy <michael@flatgalaxy.com>
2016-10-03 17:45:52 -0500

    trying to pull out pings

--- serve/mica.js
+++ serve/mica.js
@@ -1326,7 +1326,7 @@ function validatefile() {
     go([$("#fileform"), ''], '', unavailable(false), validatefile_complete, false);
 }
 
-function handleIqPing(iq) {
+function handleIqPing(oIQ) {
     //ping: N (raw): <iq xmlns="jabber:client" from="readalien.com" to="mica_admin@readalien.com/mica2247499f" id="5458144941139737802" type="get"><ping xmlns="urn:xmpp:ping"/></iq>
     console.log("Ping!!");
     con.send(oIQ.errorReply(ERR_FEATURE_NOT_IMPLEMENTED));
