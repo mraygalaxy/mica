4a4af4933ad3e491823b5ba032a0f38dad9df341
Michael R. Hines <michael@hinespot.com>
2016-11-02 14:43:28 -0500

    got the anchors right

--- serve/head.js
+++ serve/head.js
@@ -695,3 +695,30 @@ function ctest() {
 
 }
 
+
+function help_anchors() {
+  $('a.anch').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+&& location.hostname == this.hostname) {
+
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top - 55 //offsets for fixed header
+        }, 1000);
+        return false;
+      }
+    }
+  });
+  //Executed on page load with URL containing an anchor tag.
+  if($(location.href.split("#")[1])) {
+      var target = $('#'+location.href.split("#")[1]);
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top - 55 //offset height of header here too.
+        }, 1000);
+        return false;
+      }
+  }
+}
