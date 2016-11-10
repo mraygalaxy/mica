cbdf0921f1b6b924b3ae217082644ca7e9910cc4
Michael R. Hines <michael@hinespot.com>
2016-11-10 02:10:36 -0600

    try this one

--- mica.py
+++ mica.py
@@ -5040,7 +5040,7 @@ class MICA(object):
                        start = _("Start learning!"),
                        password = password,
                        firsttime = _("If this is your first time here"),
-                       welcome = _("We have created a default password to be used with your mobile device(s). Please write it down somewhere. You will need it only if you want to synchronize your mobile devices with the website. If you do not want to use the mobile application, you can ignore it. If you do not want to write it down, you will have to come back to your account preferences and reset it before trying to login to the mobile application. You are welcome to go to your preferences now and change this password."))
+                       welcome = "<div style='margin: 100px'>" + _("We have created a default password to be used with your mobile device(s). Please write it down somewhere. You will need it only if you want to synchronize your mobile devices with the website. If you do not want to use the mobile application, you can ignore it. If you do not want to write it down, you will have to come back to your account preferences and reset it before trying to login to the mobile application. You are welcome to go to your preferences now and change this password.") + "</div>")
 
             req.messages = output
         else :
