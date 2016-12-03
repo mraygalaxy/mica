de0b52dd50bfcfb4b742d53b0a5910d5cc819cc1
Michael R. Galaxy <michael@flatgalaxy.com>
2016-12-03 11:33:24 -0600

    large messages. Geeze.

--- serve/head.js
+++ serve/head.js
@@ -570,7 +570,7 @@ function ctest() {
                     if (this.is_chatroom && attrs.sender === 'them' && (new RegExp("\\b"+this.model.get('nick')+"\\b")).test(text)) {
                         extra_classes += ' mentioned';
                     }
-                    if (text.length > 10000) {
+                    if (text.length > 20000) {
                         text = text.substring(0, 10) + '...';
                         this.showStatusNotification(local("largemessage"), true, true);
                     }
