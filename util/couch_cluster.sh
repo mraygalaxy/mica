2a9fdb45df32ee3f7479262f80492311f6f6974c
Michael R. Galaxy <michael@flatgalaxy.com>
2017-05-11 15:56:40 -0500

    modifying scripts

--- util/couch_cluster.sh
+++ util/couch_cluster.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# service ssh start debugging, only
+service ssh start #debugging, only
 
 ip=$(ip addr show eth0 | grep inet | sed -e "s/ \+/ /g" | head -1 | cut -d " " -f 3 | sed -e "s/\/.*//g")
 echo "initializing couch node name with: couchdb@${ip}"
@@ -11,12 +11,20 @@ sed -ie "s/-name .*/-name couchdb@${ip}/g" /usr/local/lib/couchdb/etc/vm.args
 # way to make it forget is to blow away the data.
 rm -rf /usr/local/lib/couchdb/data/*
 
+#sleep 5
+#echo "Rebinding..."
+#curl -X PUT http://127.0.0.1:5984/_node/couchdb@${ip}/_config/chttpd/bind_address -d '"0.0.0.0"'
+#curl -X PUT http://127.0.0.1:5984/_node/couchdb@${ip}/_config/httpd/bind_address -d '"0.0.0.0"'
+
+echo "starting cluster"
 service couchdb start
 
-sleep 5
-echo "Rebinding..."
+sleep 15
 
-curl -X PUT http://127.0.0.1:5984/_node/couchdb@${ip}/_config/chttpd/bind_address -d '"0.0.0.0"'
-curl -X PUT http://127.0.0.1:5984/_node/couchdb@${ip}/_config/httpd/bind_address -d '"0.0.0.0"'
+echo "stopping cluster"
+service couchdb stop
+
+echo "Starting up..."
+(./restart.sh &)
 
 bash
