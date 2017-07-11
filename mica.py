bb0aacd1709a1b1637766ce54add57ce61718fff
Michael R. Hines <michael@hinespot.com>
2017-07-10 20:29:10 -0500

    good test. I forgot that I did this.

--- mica.py
+++ mica.py
@@ -4057,7 +4057,7 @@ class MICA(object):
 
     def render_translate(self, req, story) :
         output = ""
-        if story["translated"] :
+        if story["translated"] or story["translating"] :
             output += _("Story already translated. To re-translate, please select 'Forget'.")
         else :
             pt = Thread(target = self.parse, args = [req, story])
